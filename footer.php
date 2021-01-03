db9ab61f49325f8c6530909127b3bc1e449b4fbc
Gentille Plume <melvin.lemoine@feather-project.org>
2021-01-03 17:27:43 +0100

    Developed footer

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 <?php wp_footer(); ?>
 
-<footer></footer>
-
+<footer class="is-blue pv-3">
+  <?php echo file_get_contents('https://melvin-lemoine.me/source/credit.php'); ?>
+</footer>
 
 </body>
 </html>
