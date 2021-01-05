5bea7ef99d6095ce6aadf3a27344808b76e5a5d2
Gentille Plume <melvin.lemoine@feather-project.org>
2021-01-05 10:58:54 +0100

    jquery issue fixed + imported hightlightjs

--- functions.php
+++ functions.php
@@ -18,8 +18,11 @@ function insert_css() {
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
 
 	//########## Import all the javascript scripts here like the example below ##########
-	wp_register_script( 'jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js' );
- 	wp_enqueue_script( 'jquery' );
+	wp_register_script( 'jquery2', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js' );
+ 	wp_enqueue_script( 'jquery2' );
+
+	wp_register_script( 'highlightjs', 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.5.0/highlight.min.js' );
+ 	wp_enqueue_script( 'highlightjs' );
 }
 
 add_theme_support( 'menus' );
