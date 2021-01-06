49d23dda44a0f01a10ce398634dd82ae32216693
Gentille Plume <melvin.lemoine@feather-project.org>
2021-01-06 09:03:46 +0100

    Setted up popup system + setted up highlight js

--- footer.php
+++ footer.php
@@ -4,5 +4,43 @@
   <?php echo file_get_contents('https://melvin-lemoine.me/source/credit.php'); ?>
 </footer>
 
+<script>
+  var popupLever = false;
+  // var previous_url;
+  // var new_url;
+
+  function popupSwitch(id){
+
+    if (popupLever == false){
+      popupLever = true;
+      // previous_url = window.location.href;
+      // console.log("previous url : " + window.location.href);
+      // new_url = window.location.href + '?popuptitle=test';
+
+      // setTimeout(function(){
+        // window.history.pushState("test", "test", new_url);
+      // }, 50)
+
+
+      console.log("Switch ON popup");
+
+
+      $('#' + id).css("visibility", "visible");
+      $('html').css('overflow-y', 'hidden');
+
+    } else {
+      // window.history.pushState("test", "test", previous_url);
+      // console.log("set previous url : " + previous_url);
+      popupLever = false;
+      console.log("switch OFF popup");
+
+      $('.popup').css("visibility", "hidden");
+      $('html').css('overflow-y', 'scroll');
+    }
+  }
+</script>
+
+<script>hljs.initHighlightingOnLoad();</script>
+
 </body>
 </html>
