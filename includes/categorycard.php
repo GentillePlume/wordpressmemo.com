a0e398d5478b10e492ffd4bb88693c2cd5a718c7
Gentille Plume <melvin.lemoine@feather-project.org>
2021-01-13 09:09:14 +0100

    blog categories section is now responsive

--- includes/categorycard.php
+++ includes/categorycard.php
@@ -1,4 +1,4 @@
-<a class="categorycard flex-r wrap h-centered is-light border-5 p-3-m" href="<?php echo get_category_link( $category->term_id ) ?>">
+<a class="categorycard flex-r wrap h-centered is-light border-5 p-1-xs p-2-s p-3-m mb-1-xs" href="<?php echo get_category_link( $category->term_id ) ?>">
   <div class="categorycard__logo flex-c h-centered">
     <img class="categorycard__logo_img" src="<?php bloginfo('template_url'); ?>/images/categories/<?php echo $category->name; ?>" alt="category logo"/>
   </div>
